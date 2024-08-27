9ce4dfa1b7efd84f0ad0843d18c7a093a3f2c3f1
kedabudre <abdurehimk77@gmail.com>
2024-08-27 18:28:10 +0200

    update setup the db connection

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -1,5 +1,18 @@
 import mongoengine
 
-
 def global_init():
+    """
+    Initializes the connection to the MongoDB database using MongoEngine.
+
+    This function sets up a connection to a MongoDB database, allowing you to interact
+    with the database in your application. The connection is registered with a specific alias 
+    so it can be easily referenced later in your code.
+
+    Parameters:
+    - alias='core': A string that serves as an alias for this particular database connection.
+                    You can use this alias ('core') to refer to the connection throughout your application.
+                    
+    - name='snake_bnb': The name of the MongoDB database to which you are connecting.
+                        In this case, the database is named 'snake_bnb'.
+    """
     mongoengine.register_connection(alias='core', name='snake_bnb')
